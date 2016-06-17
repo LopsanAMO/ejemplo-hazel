92beeb9848376e4a522486abe1dbcb3b0d995223
LopsanAMO <lmolinao1200@alumno.ipn.mx>
2016-06-17 10:52:26 -0500

    Ya compila

--- loquesea/urls.py
+++ loquesea/urls.py
@@ -1,19 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from proyecto import urls as zeldas
-<<<<<<< HEAD
 from login import urls as urlsLogin
-=======
->>>>>>> 09f7e832092b35bafd11940a03f09c3871cf877e
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-<<<<<<< HEAD
     url(r'^proyecto/', include(zeldas , namespace="proyectolo")),
-    url(r'^login/', include(urlsLogin, namespace='login'))
-=======
-    url(r'^proyecto/', include(zeldas , namespace="proyectolo"))
->>>>>>> 09f7e832092b35bafd11940a03f09c3871cf877e
+    url(r'^login/', include(urlsLogin, namespace='login')),
+    url(r'^proyecto/', include(zeldas , namespace="proyectolo")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
